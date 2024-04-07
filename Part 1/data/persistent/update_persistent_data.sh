71043bec6102ef56d6c9f08b03246f3c97630a9d
C-bean <p930026016@mail.uic.edu.hk>
2024-04-07 18:32:18 +0200

    Add automate update_persistent_data.sh

--- Part 1/data/persistent/update_persistent_data.sh
+++ Part 1/data/persistent/update_persistent_data.sh
@@ -1,3 +1,9 @@
+# Run crawler.py
+python /path/to/BDM-Project/Part\ 1/crawler.py
+
+# Run Data_Persistence_Loaders.ipynb
+jupyter nbconvert --to notebook --execute //home/ecs-assist-user/BDM-Project/Part\ 1/Data_Persistence_Loaders.ipynb --output //home/ecs-assist-user/BDM-Project/Part\ 1/Data_Persistence_Loaders_executed_$(date +"%Y%m%d").ipynb
+
 # Update Persistent Landing Zone postgresql
 sudo systemctl restart postgresql
 
